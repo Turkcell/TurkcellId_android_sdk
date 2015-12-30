bb23fa1abeace608be23a845ea77fac0d6945d96
nikinci <nurullahikinci@gmail.com>
2015-12-30 11:46:21 +0200

    logout changes, key changes

--- AndroidStudioProject/app/src/main/java/com/turkcell/loginsdktest/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/turkcell/loginsdktest/MainActivity.java
@@ -3,55 +3,99 @@ package com.turkcell.loginsdktest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.turkcell.loginsdk.helper.LoginSdk;
+import com.turkcell.loginsdk.service.callback.LoginSdkCallBack;
+import com.turkcell.loginsdk.service.callback.LogoutResponse;
+
 public class MainActivity extends Activity {
+
+    private Button buttonLogout;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        login();
+
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginSdk.logOut(MainActivity.this, new LoginSdkCallBack<LogoutResponse>() {
+                    @Override
+                    public void onSuccess(LogoutResponse logoutResponse) {
+                        Toast.makeText(MainActivity.this, "Logout Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFail(Exception e) {
+                        Toast.makeText(MainActivity.this, "Logout Fail", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void login() {
         Intent intent = new Intent(this, com.turkcell.loginsdk.activity.LoginSDKMainActivity.class);
-//	intent.putExtra("isLogout", "true");
-        intent.putExtra("fontPath","fonts/LDFComicSans.ttf");
-//	intent.putExtra("Env", "PROD");//Default test
-        intent.putExtra("appId", "123");// zorunlu
-        intent.putExtra("dll", "true");// zorunlu
-        intent.putExtra("loginForce", "true");// zorunlu
-        intent.putExtra("isDirectLogin", "false");// direk wifi login sayfası açar
-        intent.putExtra("pageBackgroundColor", R.color.white);//Sayfan?n arka plan rengi icin
-        intent.putExtra("pageTextColor", R.color.lsdk_green1);//Sayfadaki textlerin renkleri
-        intent.putExtra("headerBackgroundColor", R.color.flatBlue);//Headerdaki background rengi
-        intent.putExtra("headerTextBackgroundColor", R.color.flatBlue);//Headerdaki textin arka plan rengi
-        intent.putExtra("headerTextColor", R.color.white);//Headerdaki textin rengi
-//	intent.putExtra("headerTitle", "Turkcell Platinum");//Headerdaki text
-        intent.putExtra("positiveButtonColor", R.color.flatBlue);//Positif button renkleri
-        intent.putExtra("positiveButtonTextColor", R.color.white);//Positif button text renkleri
-        intent.putExtra("negativeButtonColor", R.color.lsdk_black);//Negatif button renkleri
-        intent.putExtra("negativeButtonTextColor", R.color.white);//Negatif button text renkleri
-        intent.putExtra("inputBackgroundColor", R.color.lsdk_gray_background);//Edittextler icin background rengi
-        intent.putExtra("showFreeText", "true");//Free Texti göster
-        intent.putExtra("freeText", "bla blab blab bla bla bla");//free text alandaki text
-//	intent.putExtra("exitButtonDrawable", com.turkcell.loginsdktest.R.drawable.a);
-//	intent.putExtra("backButtonDrawable", com.turkcell.loginsdktest.R.drawable.b);
-//	intent.putExtra("checkedCheckBoxButtonDrawable", com.turkcell.loginsdktest.R.drawable.custom_icon);
-//	intent.putExtra("turkcellLogoDrawable", com.turkcell.loginsdktest.R.drawable.d);
-//	intent.putExtra("hidePreloader", "true");//Preloader ı gizle
-        intent.putExtra("hintTextColor", R.color.lsdk_yellow1);//Edittext hintlerin rengi
-//	intent.putExtra("captchaButtonDrawable", R.drawable.lsdk_icon_refresh);
+
+        //logout call changed to LoginSdk.logout(context,LoginSdkCallback);
+        //intent.putExtra(LoginSdk.KEY_IS_LOGOUT, "true");
+        intent.putExtra(LoginSdk.KEY_FONT_PATH, "fonts/LDFComicSans.ttf");
+        //intent.putExtra(LoginSdk.KEY_ENV, LoginSdk.KEY_ENV_PROD);//Default test
+        intent.putExtra(LoginSdk.KEY_APP_ID, "123");// zorunlu
+        intent.putExtra(LoginSdk.KEY_DLL, "true");// zorunlu
+        //loginforce change to ShowLoginPage
+        intent.putExtra(LoginSdk.KEY_SHOW_LOGIN_PAGE, "true");// zorunlu
+        intent.putExtra(LoginSdk.KEY_IS_DIRECT_LOGIN, "false");// direk wifi login sayfası açar
+        intent.putExtra(LoginSdk.KEY_PAGE_BACKGROUND_COLOR, R.color.white);//Sayfan?n arka plan rengi icin
+        intent.putExtra(LoginSdk.KEY_PAGE_TEXT_COLOR, R.color.lsdk_green1);//Sayfadaki textlerin renkleri
+        intent.putExtra(LoginSdk.KEY_HEADER_BACKGROUND_COLOR, R.color.flatBlue);//Headerdaki background rengi
+        intent.putExtra(LoginSdk.KEY_HEADER_TEXT_BACKGROUND_COLOR, R.color.flatBlue);//Headerdaki textin arka plan rengi
+        intent.putExtra(LoginSdk.KEY_HEADER_TEXT_COLOR, R.color.white);//Headerdaki textin rengi
+        //intent.putExtra(LoginSdk.KEY_HEADER_TITLE, "Turkcell Platinum");//Headerdaki text
+        intent.putExtra(LoginSdk.KEY_POSITIVE_BUTTON_COLOR, R.color.flatBlue);//Positif button renkleri
+        intent.putExtra(LoginSdk.KEY_POSITIVE_BUTTON_TEXT_COLOR, R.color.white);//Positif button text renkleri
+        intent.putExtra(LoginSdk.KEY_NEGATICE_BUTTON_COLOR, R.color.lsdk_black);//Negatif button renkleri
+        intent.putExtra(LoginSdk.KEY_NEGATICE_BUTTON_TEXT_COLOR, R.color.white);//Negatif button text renkleri
+        intent.putExtra(LoginSdk.KEY_INPUT_BACKGROUND_COLOR, R.color.lsdk_gray_background);//Edittextler icin background rengi
+        intent.putExtra(LoginSdk.KEY_SHOW_FREE_TEXT, "true");//Free Texti göster
+        intent.putExtra(LoginSdk.KEY_FREE_TEXT, "bla blab blab bla bla bla");//free text alandaki text
+        //intent.putExtra(LoginSdk.KEY_EXIT_BUTTON_DRAWABLE, com.turkcell.loginsdktest.R.drawable.a);
+        //intent.putExtra(LoginSdk.KEY_BACK_BUTTON_DRAWABLE, com.turkcell.loginsdktest.R.drawable.b);
+        //intent.putExtra(LoginSdk.KEY_CHECKHED_CHECKBOX_BUTTON_DRAWABLE, com.turkcell.loginsdktest.R.drawable.custom_icon);
+        //intent.putExtra(LoginSdk.KEY_TURKCELL_LOGO_DRAWABLE, com.turkcell.loginsdktest.R.drawable.d);
+        //intent.putExtra(LoginSdk.KEY_HIDE_PRELOADER, "true");//Preloader ı gizle
+        intent.putExtra(LoginSdk.KEY_HINT_TEXT_COLOR, R.color.lsdk_yellow1);//Edittext hintlerin rengi
+        //intent.putExtra(LoginSdk.KEY_CAPTCHA_BUTTON_DRAWABLE, R.drawable.lsdk_icon_refresh);
 
         startActivityForResult(intent, 100);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String resultMessage = data.getStringExtra("authToken") == null ? data.getStringExtra("code") + "_" + data.getStringExtra("message") : data.getStringExtra("authToken");
+        String resultMessage = data.getStringExtra(LoginSdk.KEY_AUTH_TOKEN) == null ? data.getStringExtra(LoginSdk.KEY_CODE) + "_" + data.getStringExtra(LoginSdk.KEY_MESSAGE) : data.getStringExtra(LoginSdk.KEY_AUTH_TOKEN);
         if (100 == requestCode) {
             if (resultCode == Activity.RESULT_OK) {
-                Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show();
+                Toast.makeText(this, resultMessage, Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show();
+                Toast.makeText(this, resultMessage, Toast.LENGTH_SHORT).show();
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
