e6d166e4d1b163babf904311f54f594a3020b925
tnikinci <nurullah.ikinci@consultant.turkcell.com.tr>
2016-01-11 16:06:36 +0200

    keys update.fixes

---
+++ LibraryProject/LoginSDKTest/gen/com/turkcell/loginsdktest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.turkcell.loginsdktest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- LibraryProject/LoginSDKTest/src/com/turkcell/loginsdktest/MainActivity.java
+++ LibraryProject/LoginSDKTest/src/com/turkcell/loginsdktest/MainActivity.java
@@ -3,57 +3,155 @@ package com.turkcell.loginsdktest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.turkcell.loginsdk.helper.LoginSdk;
+import com.turkcell.loginsdk.service.callback.LoginSdkCallBack;
+import com.turkcell.loginsdk.service.callback.LogoutResponse;
 
 public class MainActivity extends Activity {
 
+    private Button buttonLogin3g;
+    private Button buttonLoginWithout3g;
+    private Button buttonLogout;
+    private TextView textView;
+    private static final int LOGINSDK_REQUEST_CODE = 111;
+    private String appId = "11111";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-
-    setContentView(com.turkcell.loginsdktest.R.layout.activity_main);
-    Intent intent = new Intent(this, com.turkcell.loginsdk.activity.LoginSDKMainActivity.class);
-//intent.putExtra("isLogout", "true");
-    intent.putExtra("fontPath","fonts/LDFComicSans.ttf");
-//intent.putExtra("Env", "PROD");//Default test
-    intent.putExtra("appId", "123");// zorunlu
-	intent.putExtra("dll", "false");// zorunlu
-	intent.putExtra("loginForce", "false");// zorunlu
-	intent.putExtra("isDirectLogin", "true");// direk wifi login sayfasi acar
-    intent.putExtra("pageBackgroundColor", R.color.white);//Sayfanin arka plan rengi icin
-    intent.putExtra("pageTextColor", R.color.lsdk_gray1);//Sayfadaki textlerin renkleri
-    intent.putExtra("headerBackgroundColor", R.color.flatBlue);//Headerdaki background rengi
-    intent.putExtra("headerTextBackgroundColor", R.color.flatBlue);//Headerdaki textin arka plan rengi
-    intent.putExtra("headerTextColor", R.color.white);//Headerdaki textin rengi
-//	intent.putExtra("headerTitle", "Turkcell Platinum");//Headerdaki text
-    intent.putExtra("positiveButtonColor", R.color.flatBlue);//Positif button renkleri
-    intent.putExtra("positiveButtonTextColor", R.color.white);//Positif button text renkleri
-    intent.putExtra("negativeButtonColor", R.color.white);//Negatif button renkleri
-    intent.putExtra("negativeButtonTextColor", R.color.lsdk_gray1);//Negatif button text renkleri
-    intent.putExtra("inputBackgroundColor", R.color.lsdk_gray_background);//Edittextler icin background rengi
-	intent.putExtra("showFreeText", "true");//Free Texti goster
-//	intent.putExtra("freeText", "bla blab blab bla bla bla");//free text alandaki text
-//intent.putExtra("exitButtonDrawable", com.turkcell.loginsdktest.R.drawable.a);
-//intent.putExtra("backButtonDrawable", com.turkcell.loginsdktest.R.drawable.b);
-//intent.putExtra("checkedCheckBoxButtonDrawable", com.turkcell.loginsdktest.R.drawable.custom_icon);
-//intent.putExtra("turkcellLogoDrawable", com.turkcell.loginsdktest.R.drawable.d);
-//	intent.putExtra("hidePreloader", "true");//Preloader ˝ gizle
-	intent.putExtra("hintTextColor", R.color.lsdk_yellow1);//Edittext hintlerin rengi
-//	intent.putExtra("captchaButtonDrawable", R.drawable.lsdk_icon_refresh);
-
-	startActivityForResult(intent, 100);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setButtons();
     }
+
+    private void setButtons() {
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("AppId : " + appId);
+        buttonLogin3g = (Button) findViewById(R.id.buttonLogin3g);
+        buttonLoginWithout3g = (Button) findViewById(R.id.buttonLoginWithout3g);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        buttonLogin3g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivityForResult(getLogin3gIntent(), LOGINSDK_REQUEST_CODE);
+            }
+        });
+        buttonLoginWithout3g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(getLoginWithout3gIntent(), LOGINSDK_REQUEST_CODE);
+
+            }
+        });
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginSdk.logOut(MainActivity.this, new LoginSdkCallBack<LogoutResponse>() {
+                    @Override
+                    public void onSuccess(LogoutResponse value) {
+                        Toast.makeText(MainActivity.this, "logout Success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFail(Exception exception) {
+                        Toast.makeText(MainActivity.this, "logout failed", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+
+
+    }
+
+    private Intent getLogin3gIntent() {
+        Intent intent = new Intent(this, com.turkcell.loginsdk.activity.LoginSDKMainActivity.class);
+        intent.putExtra("dll", "false");// zorunlu
+        intent.putExtra("isDirectLogin", "false");// zorunlu
+        setIntentDefValues(intent);
+        return intent;
+    }
+
+    private Intent getLoginWithout3gIntent() {
+        Intent intent = new Intent(this, com.turkcell.loginsdk.activity.LoginSDKMainActivity.class);
+        intent.putExtra("dll", "true");// zorunlu
+        intent.putExtra(LoginSdk.KEY_SHOW_LOGIN_PAGE, "true");// zorunlu
+        intent.putExtra("isDirectLogin", "false");// zorunlu
+        setIntentDefValues(intent);
+        return intent;
+    }
+
+
+    private Intent getLsdkIntent() {
+        Intent intent = new Intent(this, com.turkcell.loginsdk.activity.LoginSDKMainActivity.class);
+
+        intent.putExtra("dll", "true");// zorunlu
+        intent.putExtra(LoginSdk.KEY_SHOW_LOGIN_PAGE, "true");// zorunlu
+        intent.putExtra("isDirectLogin", "true");// zorunlu
+
+        setIntentDefValues(intent);
+
+        return intent;
+    }
+
+    private void setIntentDefValues(Intent intent) {
+        intent.putExtra("appId", appId);// zorunlu
+        intent.putExtra(LoginSdk.KEY_SHOW_LOGIN_PAGE, "true");// zorunlu
+        intent.putExtra("pageBackgroundColor", R.color.white);//Sayfan?n arka plan rengi icin
+        intent.putExtra("pageTextColor", R.color.lsdk_gray1);//Sayfadaki textlerin renkleri
+        intent.putExtra("headerBackgroundColor", R.color.flatBlue);//Headerdaki background rengi
+        intent.putExtra("headerTextBackgroundColor", R.color.flatBlue);//Headerdaki textin arka plan rengi
+        intent.putExtra("headerTextColor", R.color.white);//Headerdaki textin rengi
+        intent.putExtra("headerTitle", "Turkcell Platinum");//Headerdaki text
+        intent.putExtra("positiveButtonColor", R.color.flatBlue);//Positif button renkleri
+        intent.putExtra("positiveButtonTextColor", R.color.white);//Positif button text renkleri
+        intent.putExtra("negativeButtonColor", R.color.black);//Negatif button renkleri
+        intent.putExtra("negativeButtonTextColor", R.color.white);//Negatif button text renkleri
+        intent.putExtra("inputBackgroundColor", R.color.lsdk_gray_background);//Edittextler icin background rengi
+        //	intent.putExtra("Env", "PROD");//Default test
+         intent.putExtra(LoginSdk.KEY_FONT_PATH, "fonts/LDFComicSansBold.ttf");
+        //	intent.putExtra("exitButtonDrawable", com.turkcell.loginsdktest.R.drawable.a);
+//	intent.putExtra("backButtonDrawable", com.turkcell.loginsdktest.R.drawable.b);
+//	intent.putExtra("checkedCheckBoxButtonDrawable", com.turkcell.loginsdktest.R.drawable.custom_icon);
+//	intent.putExtra("turkcellLogoDrawable", com.turkcell.loginsdktest.R.drawable.d);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	String resultMessage = data.getStringExtra("authToken") == null ? data.getStringExtra("code") + "_" + data.getStringExtra("message") : data.getStringExtra("authToken");
-	if (100 == requestCode) {
-	    if (resultCode == Activity.RESULT_OK) {
-		Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show();
-	    } else {
-		Toast.makeText(this, resultMessage, Toast.LENGTH_LONG).show();
-	    }
-	}
-	super.onActivityResult(requestCode, resultCode, data);
+
+        if (LOGINSDK_REQUEST_CODE == requestCode) {
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String status = data.getStringExtra("status") != null ? data.getStringExtra("status") : "";
+            String message = data.getStringExtra("message") != null ? data.getStringExtra("message") : "";
+            String loginType = data.getStringExtra("loginType") != null ? data.getStringExtra("loginType") : "";
+            String authToken = data.getStringExtra("authToken") != null ? data.getStringExtra("authToken") : "";
+
+            stringBuilder.append("status: " + status);
+            stringBuilder.append("\n");
+            stringBuilder.append("message: " + message);
+            stringBuilder.append("\n");
+            stringBuilder.append("loginType: " + loginType);
+            stringBuilder.append("\n");
+            stringBuilder.append("authToken: " + authToken);
+            stringBuilder.append("\n");
+
+            if (resultCode == Activity.RESULT_OK) {
+                stringBuilder.append("RESULT:OK \n");
+
+
+            } else {
+                stringBuilder.append("RESULT:NOT-OK \n");
+            }
+
+            textView.setText(stringBuilder.toString());
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
